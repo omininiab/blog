cf129bc52605f14a44d1db330b5cf4fe36ca329c
Ominini <ominini@duck.com>
2024-01-05 14:29:55 -0500

    bug fix in js

--- business/index.js
+++ business/index.js
@@ -21,7 +21,7 @@ const posts = [
         "published": "04-Jan-2024",
         "updated": "04-Jan-2024"
     }, {
-        "url": "/blogpost.html",
+        "url": "blogpost.html",
         "author": "Ominini Ambie-Barango",
         "title": "Blog Title",
         "summary": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam praesentium maiores aspernatur, quam quae animi vel mollitia harum quas. Quis aut dolore eaque repellendus corporis at quas expedita ipsa architecto.",
@@ -32,9 +32,9 @@ const posts = [
 
 
 let text = "";
+console.log("Script loaded.");
 for (let x of posts) {
-
-    text += "<hr/><a href = \"" + x[url] + "\"><h3>" + x["title"] + "</h3></a><p>" + x["summary"] + "</p><b>" + x["author"] + " | " + x["published"] + "</b>";
+    text += "<hr/><a href = \"" + x[url] + "\"><h3>" + x["title"] + "</h3></a><p>" + x["summary"] + "</p><b>by: " + x["author"] + " | " + x["published"] + "</b>";
 }
 const bloglist = document.querySelector("#main");
 bloglist.innerHTML = text;
