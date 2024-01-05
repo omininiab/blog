942cac70528d8ce8e849952cabe6473c31625a50
Ominini <ominini@duck.com>
2024-01-05 14:30:35 -0500

    bug fix in js

--- business/index.js
+++ business/index.js
@@ -1,20 +1,20 @@
 const posts = [
     {
-        "url": "/blogpost.html",
+        "url": "blogpost.html",
         "author": "Ominini Ambie-Barango",
         "title": "Blog Title",
         "summary": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam praesentium maiores aspernatur, quam quae animi vel mollitia harum quas. Quis aut dolore eaque repellendus corporis at quas expedita ipsa architecto.",
         "published": "04-Jan-2024",
         "updated": "04-Jan-2024"
     }, {
-        "url": "/blogpost.html",
+        "url": "blogpost.html",
         "author": "Ominini Ambie-Barango",
         "title": "Blog Title",
         "summary": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam praesentium maiores aspernatur, quam quae animi vel mollitia harum quas. Quis aut dolore eaque repellendus corporis at quas expedita ipsa architecto.",
         "published": "04-Jan-2024",
         "updated": "04-Jan-2024"
     }, {
-        "url": "/blogpost.html",
+        "url": "blogpost.html",
         "author": "Ominini Ambie-Barango",
         "title": "Blog Title",
         "summary": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam praesentium maiores aspernatur, quam quae animi vel mollitia harum quas. Quis aut dolore eaque repellendus corporis at quas expedita ipsa architecto.",
